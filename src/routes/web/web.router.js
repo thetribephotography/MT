5ed86f2ac98d8e479e9939ad0b2aa98272c58a6b
Egbeleke Daniel <www.daniko15@gmail.com>
2022-11-02 04:14:58 +0100

    routes

--- src/routes/web/web.router.js
+++ src/routes/web/web.router.js
@@ -1,4 +1,15 @@
-const Router = require ('express');
+
 const siteRouter = require('./site.router');
 const adminRouter = require('./admin.router');
 const userRouter = require('./user.router');
+
+const helpers = require('../../helpers/auth.guard')
+
+
+const webRouter = require('express').Router();
+
+webRouter.use('/admin', helpers.auth, adminRouter);
+webRouter.use('/user', helpers.auth, userRouter);
+webRouter.use('/', siteRouter);
+
+module.exports = webRouter;
