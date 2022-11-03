3ce68e861ab4d3a8e26c82c061c8be21e631585e
Egbeleke Daniel <www.daniko15@gmail.com>
2022-11-03 22:29:32 +0100

    auth error fix

--- src/routes/web/site.router.js
+++ src/routes/web/site.router.js
@@ -2,3 +2,10 @@ const siteController = require('../../controllers/site.controller');
 const helpers = require('../../helpers/auth.guard')
 const siteRouter = require('express').Router();
 
+siteRouter.get('/', siteController.login)
+siteRouter.post('/',  helpers.auth('local', {
+    failureRedirect : "/",
+    failureFlash : true
+}),(req, res)=>{
+    helpers.redirect(req, res, req.user.Role.name)
+} )
